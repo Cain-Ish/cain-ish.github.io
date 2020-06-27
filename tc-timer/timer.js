90ecd454c60fc795905dd643feb9f7b200b73e4b
Cain-Ish <mariusz.machuta@gmail.com>
2020-06-28 00:06:55 +0200

    TC Timer

--- tc-timer/timer.js
+++ tc-timer/timer.js
@@ -1,63 +1,98 @@
-// Ranek - 15 minut, powolne rozjaśnianie się (sklepy otwarte)
-// Dzień - 105 minut (sklepy otwarte)
-// Wieczór - 15 minut - powolne ściemnianie się (sklepy otwarte)
-// Noc - 45 minute (sklepy zamknięte)
-// Dobra łacznie: 3 godziny
+// 19:50 polnoc
+// 19:57 środek nocy
+// 20:20 Nad ranem + brama otwarta
+// 20:44 > Sklep slums
+// 20:47 > Sklep miasto
+// 20:51 Poranek
+// 21:21 Południe
+// 21:28 Wczesne popołudnie
+// 21:48 > Resupply
+// 21:51 Późne popołudnie
+// 22:xx wieczór(?)
+// 22:21 Późny wieczór
+// 22:36 > sklep close + brama
+// 22:51 Polnoc
+// 22:57 Środek Nocy
+// 23:20 Nad ranem
 
-// 16:40 - późny wieczór
-// 16:50 - połnoc
-
-let hour, minutes;
-let myDate = new Date();
-const seconds = myDate.getSeconds();
+const setTimeoutSecondsLeft = new Date().getSeconds() * 1000;
+const deSyncMinutes = 40; //Minutes to add/take to have full hour like 20:00
 
 const dayState = {
-	morning: 'morning',
-	day: 'day',
-	dawn: 'dawn',
-	night: 'night',
+	name: '---',
+	shop_slums: true,
+	shop_city: true,
+	gate: true,
+	resupply: false,
 };
 
-let dayPart = dayState.morning;
-
-const dayNames = {
-	morning: 'Ranek (sklepy otwarte)',
-	day: 'Dzień (sklepy otwarte)',
-	dawn: 'Wieczór (sklepy otwarte)',
-	night: 'Noc (sklepy zamkniete)',
+const dayTimeline = {
+	0: { name: 'Nad ranem', gate: true }, //0:00
+	24: { shop_slums: true }, //0:24
+	27: { shop_city: true }, //0:27
+	31: { name: 'Poranek' }, //0:31
+	61: { name: 'Południe' }, //1:01
+	68: { name: 'Wczesne popołudnie' }, //1:08
+	88: { resupply: true }, //1:28
+	68: { name: 'Wczesne popołudnie' }, //1:31
+	//0: { name: 'Wczesny wieczór' }, // ???
+	121: { name: 'Późny wieczór' }, //2:01
+	136: { shop_slums: false, shop_city: false, gate: false },
+	151: { name: 'Połnoc' }, //2:31
+	157: { name: 'Środek nocy' }, //2:37
 };
 
-function timer() {
-	console.log('timer');
-	myDate = new Date();
-	hour = myDate.getHours();
-	minutes = myDate.getMinutes();
+function calcHours(hourPast, minutes) {
+	let countDate = new Date();
+	countDate.setHours(countDate.getHours() - hourPast);
+	countDate.setMinutes(minutes);
+	return ('00' + countDate.getHours()).slice(-2) + ':' + ('00' + countDate.getMinutes()).slice(-2);
+}
 
-	let hourPart = hour % 3;
-	let day = Math.floor(hour / 3);
+function dayCycle() {
+	const currentTime = new Date();
+	let desyncTime = new Date();
+	desyncTime.setMinutes(desyncTime.getMinutes() + deSyncMinutes);
+	const currentHour = desyncTime.getHours();
+	const currentMintes = desyncTime.getMinutes();
+	const ingameDate = Math.floor(desyncTime.getHours() / 3) + 1;
+	const hourPart = currentHour % 3;
+	const timeCheck = hourPart * 60 + currentMintes;
 
-	if (hourPart == 0) dayPart = minutes <= 15 ? dayState.morning : dayState.day;
-	else if (hourPart == 1) dayPart = dayState.day;
-	else if (hourPart == 2) dayPart = minutes <= 15 ? dayState.dawn : dayState.night;
+	let currentTimeline = { ...dayState };
 
-	let tillNight =
-		dayPart === dayState.night
-			? 'Noc trwa'
-			: 'Noc za: ' + (60 - minutes - 45 < 0) ? `${1 - hourPart}h` : `${120 - minutes - 45}min`;
-	let tillNewDay = `${2 - hourPart}h ${60 - minutes}min`;
+	for (const [ key, value ] of Object.entries(dayTimeline)) {
+		if (timeCheck >= key) {
+			currentTimeline = { ...currentTimeline, ...value };
+		}
+	}
 
-	document.getElementById('date').innerHTML = `Dzień ${day} - ${dayNames[
-		dayPart
-	]}<br/>${tillNight}<br/>Nowy dzień za: ${tillNewDay}`;
+	document.getElementById('currentTime').innerHTML = `<b>Godzina: ${('00' + currentTime.getHours()).slice(
+		-2,
+	)}:${('00' + currentTime.getMinutes()).slice(-2)}</b>`;
+	document.getElementById('ingameDate').innerHTML = `<b>Dzień:</b> ${ingameDate}`;
+	document.getElementById('name').innerHTML = `<b>Pora:</b> ${currentTimeline.name}`;
+	document.getElementById('shop_slums').innerHTML = `<b>Sklepy w slumsach:</b> ${currentTimeline.shop_slums
+		? `Otwarte (Zamknięcie o ${calcHours(hourPart, 136)})`
+		: `Zamknięte (Otwarcie o ${calcHours(hourPart, 3 * 60 + 24)})`}`;
+	document.getElementById('shop_city').innerHTML = `<b>Sklepy w mieście:</b> ${currentTimeline.shop_city
+		? `Otwarte (Zamknięcie o ${calcHours(hourPart, 136)})`
+		: `Zamknięte (Otwarcie o ${calcHours(hourPart, 3 * 60 + 27)})`}`;
+	document.getElementById('resupply').innerHTML = `<b>Dzienna dostawa do sklepów:</b> ${currentTimeline.resupply
+		? 'Była'
+		: 'Będzie'} o ${calcHours(hourPart, 88)}`;
+	document.getElementById('gate').innerHTML = `<b>Brama wschodnia:</b> ${currentTimeline.gate
+		? `Otwarta (Zamknięcie o ${calcHours(hourPart, 136)})`
+		: `Zamknięta (Otwarcie o ${calcHours(hourPart, 3 * 60)})`}`;
 }
 
 function TimerInvterval() {
-	setInterval(timer, 60000);
+	setInterval(dayCycle, 60000);
 }
 
 // Run script every full minute pass
 setTimeout(function() {
 	TimerInvterval();
-}, 10000 - seconds * 1000);
+}, 10000 - setTimeoutSecondsLeft);
 
-timer();
+dayCycle();
