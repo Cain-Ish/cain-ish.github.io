342baca8ccd7445d78fa23acaee1c80e36bee83d
Cain-Ish <mariusz.machuta@gmail.com>
2020-06-28 11:37:14 +0200

    TC Timer

--- tc-timer/timer.js
+++ tc-timer/timer.js
@@ -1,13 +1,6 @@
 const setTimeoutSecondsLeft = new Date().getSeconds() * 1000;
-const deSyncMinutes = 40; //Minutes to add/take to have full hour like 20:00
-
-const dayState = {
-	name: '---',
-	shop_slums: true,
-	shop_city: true,
-	gate: true,
-	resupply: false,
-};
+let deSyncMinutes = -50; //Minutes to add/take to have full hour like 20:00
+let timeRestartStarted;
 
 const dayTimeline = {
 	0: { name: 'Nad ranem', gate: true }, //0:00
@@ -16,7 +9,8 @@ const dayTimeline = {
 	31: { name: 'Poranek' }, //0:31
 	61: { name: 'Południe' }, //1:01
 	68: { name: 'Wczesne popołudnie' }, //1:08
-	88: { resupply: true }, //1:28
+	84: { resupply_slums: true }, //1:24
+	87: { resupply_city: true }, //1:27
 	98: { name: 'Późne popołudnie' }, //1:31
 	121: { name: 'Późny wieczór' }, //2:01
 	136: { shop_slums: false, shop_city: false, gate: false }, //2:16
@@ -24,48 +18,105 @@ const dayTimeline = {
 	157: { name: 'Środek nocy' }, //2:37
 };
 
-function calcHours(hourPast, minutes) {
-	let countDate = new Date();
-	countDate.setHours(countDate.getHours() - hourPast);
-	countDate.setMinutes(minutes);
+function fillText(id, text) {
+	document.getElementById(id).innerHTML = text;
+}
+
+function fillList(text) {
+	let li = document.createElement('li');
+	li.innerHTML = text;
+	document.getElementById('timeline').appendChild(li);
+}
+
+document.getElementById('deSyncMinutes').value = deSyncMinutes;
+function deSyncMinutesManual() {
+	deSyncMinutes = Number(document.getElementById('deSyncMinutes').value);
+	dayCycle();
+}
+
+function calcHours(date, hourPast, minutes) {
+	let countDate = new Date(date.getTime());
+	countDate.setHours(countDate.getHours() - Number(hourPast));
+	countDate.setMinutes(countDate.getMinutes() + Number(minutes));
 	return ('00' + countDate.getHours()).slice(-2) + ':' + ('00' + countDate.getMinutes()).slice(-2);
 }
 
 function dayCycle() {
 	const currentTime = new Date();
 	let desyncTime = new Date();
-	desyncTime.setMinutes(desyncTime.getMinutes() + deSyncMinutes);
+	timeRestartStarted = new Date();
+	desyncTime.setMinutes(desyncTime.getMinutes() - deSyncMinutes);
 	const currentHour = desyncTime.getHours();
 	const currentMintes = desyncTime.getMinutes();
 	const ingameDate = Math.floor(desyncTime.getHours() / 3) + 1;
+	let ingameDayPart = 'Nad ranem';
 	const hourPart = currentHour % 3;
 	const timeCheck = hourPart * 60 + currentMintes;
 
-	let currentTimeline = { ...dayState };
+	timeRestartStarted = desyncTime;
+	timeRestartStarted.setMinutes(0 - hourPart * 60 + deSyncMinutes);
+
+	//clear list
+	const ul = document.getElementById('timeline');
+	while (ul.firstChild) {
+		ul.removeChild(ul.firstChild);
+	}
 
 	for (const [ key, value ] of Object.entries(dayTimeline)) {
-		if (timeCheck >= key) {
-			currentTimeline = { ...currentTimeline, ...value };
+		if (timeCheck > Number(key)) {
+			if (value.name) {
+				ingameDayPart = value.name;
+				fillList(`<i>${value.name}: ${calcHours(desyncTime, hourPart, Number(key) + 180)}</i>`);
+			}
+			if (value.gate !== undefined && value.gate)
+				fillList(`Brama otwarta: ${calcHours(desyncTime, hourPart, Number(key) + 180)}`);
+			if (value.gate !== undefined && !value.gate)
+				fillList(`Brama zamknięta: ${calcHours(desyncTime, hourPart, Number(key) + 180)}`);
+			if (value.shop_slums !== undefined && value.shop_slums)
+				fillList(`Sklepy (slumsy) otwarte: ${calcHours(desyncTime, hourPart, Number(key) + 180)}`);
+			if (value.shop_slums !== undefined && !value.shop_slums)
+				fillList(`Sklepy (slumsy) zamknięte: ${calcHours(desyncTime, hourPart, Number(key) + 180)}`);
+			if (value.shop_city !== undefined && value.shop_city)
+				fillList(`Sklepy (miasto) otwarte: ${calcHours(desyncTime, hourPart, Number(key) + 180)}`);
+			if (value.shop_city !== undefined && !value.shop_city)
+				fillList(`Sklepy (miasto) zamknięte: ${calcHours(desyncTime, hourPart, Number(key) + 180)}`);
+			if (value.resupply_slums)
+				fillList(`Sklepy(slumsy) dostawa: ${calcHours(desyncTime, hourPart, Number(key) + 180)}`);
+			if (value.resupply_city)
+				fillList(`Sklepy(miasto) dostawa: ${calcHours(desyncTime, hourPart, Number(key) + 180)}`);
+		} else {
+			if (value.name) fillList(`<b><i>${value.name}: ${calcHours(desyncTime, hourPart, key)}</i></b>`);
+			if (value.gate !== undefined && value.gate)
+				fillList(`<b>Brama otwarta: ${calcHours(desyncTime, hourPart, Number(key))}</b>`);
+			if (value.gate !== undefined && !value.gate)
+				fillList(`<b>Brama zamknięta: ${calcHours(desyncTime, hourPart, Number(key))}</b>`);
+			if (value.shop_slums !== undefined && value.shop_slums)
+				fillList(`<b>Sklepy (slumsy) otwarte: ${calcHours(desyncTime, hourPart, Number(key))}</b>`);
+			if (value.shop_slums !== undefined && !value.shop_slums)
+				fillList(`<b>Sklepy (slumsy) zamknięte: ${calcHours(desyncTime, hourPart, Number(key))}</b>`);
+			if (value.shop_city !== undefined && value.shop_city)
+				fillList(`<b>Sklepy (miasto) otwarte: ${calcHours(desyncTime, hourPart, Number(key))}</b>`);
+			if (value.shop_city !== undefined && !value.shop_city)
+				fillList(`<b>Sklepy (miasto) zamknięte: ${calcHours(desyncTime, hourPart, Number(key))}</b>`);
+			if (value.resupply_slums)
+				fillList(`<b>Sklepy(slumsy) dostawa: ${calcHours(desyncTime, hourPart, Number(key))}</b>`);
+			if (value.resupply_city)
+				fillList(`<b>Sklepy(miasto) dostawa: ${calcHours(desyncTime, hourPart, Number(key))}</b>`);
 		}
 	}
 
-	document.getElementById('currentTime').innerHTML = `<b>Godzina: ${('00' + currentTime.getHours()).slice(
-		-2,
-	)}:${('00' + currentTime.getMinutes()).slice(-2)}</b>`;
-	document.getElementById('ingameDate').innerHTML = `<b>Dzień:</b> ${ingameDate}`;
-	document.getElementById('name').innerHTML = `<b>Pora:</b> ${currentTimeline.name}`;
-	document.getElementById('shop_slums').innerHTML = `<b>Sklepy w slumsach:</b> ${currentTimeline.shop_slums
-		? `Otwarte (Zamknięcie o ${calcHours(hourPart, 136)})`
-		: `Zamknięte (Otwarcie o ${calcHours(hourPart, 3 * 60 + 24)})`}`;
-	document.getElementById('shop_city').innerHTML = `<b>Sklepy w mieście:</b> ${currentTimeline.shop_city
-		? `Otwarte (Zamknięcie o ${calcHours(hourPart, 136)})`
-		: `Zamknięte (Otwarcie o ${calcHours(hourPart, 3 * 60 + 27)})`}`;
-	document.getElementById('resupply').innerHTML = `<b>Dzienna dostawa do sklepów:</b> ${currentTimeline.resupply
-		? 'Była'
-		: 'Będzie'} o ${calcHours(hourPart, 88)}`;
-	document.getElementById('gate').innerHTML = `<b>Brama wschodnia:</b> ${currentTimeline.gate
-		? `Otwarta (Zamknięcie o ${calcHours(hourPart, 136)})`
-		: `Zamknięta (Otwarcie o ${calcHours(hourPart, 3 * 60)})`}`;
+	fillText(
+		'currentTime',
+		`<b>Godzina: ${('00' + currentTime.getHours()).slice(-2)}:${('00' + currentTime.getMinutes()).slice(-2)}</b>`,
+	);
+
+	fillText(
+		'timeRestartStarted',
+		`<b>Nad ranem było o ${('00' + timeRestartStarted.getHours()).slice(-2)}:${('00' +
+			timeRestartStarted.getMinutes()).slice(-2)}</b>`,
+	);
+	fillText('ingameDate', `<b>Dzień:</b> ${ingameDate}`);
+	fillText('name', `<b>Pora:</b> ${ingameDayPart}`);
 }
 
 function TimerInvterval() {
